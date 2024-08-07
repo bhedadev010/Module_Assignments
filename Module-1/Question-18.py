4f5fc30892cfadfc6010ffcb714a9f562437d7ef
Dev Bheda <bhedadev010@gmail.com>
2024-08-07 08:18:48 +0530

    7-8-24

---
+++ Module-1/Question-18.py
@@ -0,0 +1,9 @@
+#18. Python Program to Find Factorial of Number Using Recursion
+
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+print(factorial(5))
