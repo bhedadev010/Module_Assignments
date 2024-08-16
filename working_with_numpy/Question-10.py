fc96be0bfd13f720dfcc1dfd59941e74c81e0578
Dev Bheda <bhedadev010@gmail.com>
2024-08-16 18:17:18 +0530

    16-8-24

---
+++ working_with_numpy/Question-10.py
@@ -0,0 +1,8 @@
+# Create a 2D NumPy array with shape (3, 3) containing random
+# floating-point numbers between 0 and 1.
+
+import numpy as np
+
+array = np.random.random(size=(3,3))
+
+print(array)
